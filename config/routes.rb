f0075e80fd56adb0a7b4200b740264ac9ef62790
coleman gidcumb <colemangidcumb@gmail.com>
2021-03-04 23:03:31 -0700

    eod 3-3-21

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :cocktails, only: %w[index new show create] do
     resources :doses, only: %w[new create]
   end
-  resources :doses, only: ['destroy']
+  delete 'doses/:id', to: 'doses#destroy', as: :destroy
 end
